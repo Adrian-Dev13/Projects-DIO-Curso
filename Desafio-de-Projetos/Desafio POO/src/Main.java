05ee7d7ca859b1dbb8da16d3dda21894e108ea5b
Adrian-Dev13 <pauloadrian1370@gmail.com>
2025-08-03 21:42:50 -0300

    Add files via upload

---
+++ Desafio-de-Projetos/Desafio POO/src/Main.java
@@ -0,0 +1,45 @@
+import br.com.dio.desafio.dominio.Bootcamp;
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição curso Java");
+        curso1.setCargaHoraria(12);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso Js");
+        curso2.setDescricao("Descrição curso Js");
+        curso2.setCargaHoraria(8);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria Java");
+        mentoria.setDescricao("Descrição mentoria Java");
+        mentoria.setData(LocalDate.now());
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java developer");
+        bootcamp.setDescricao("Descrição bootcamp Java developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devPaulo = new Dev();
+        devPaulo.setNome("Paulo");
+        devPaulo.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos Paulo:"+ devPaulo.getConteudosInscritos());
+        devPaulo.progredir();
+        devPaulo.progredir();
+        devPaulo.progredir();
+        System.out.println("================");
+        System.out.println("Conteúdos inscritos Paulo:"+ devPaulo.getConteudosInscritos());
+        System.out.println("Conteúdos concluidos Paulo:"+ devPaulo.getConteudosConcluidos());
+        System.out.println("XP: "+ devPaulo.calcularTotalXp());
+    }
+}
